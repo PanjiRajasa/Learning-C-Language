dc661a1f62929264f0a1e25ab91bfb4661494601
I Gusti Panji Rajasa <112536060+PanjiRajasa@users.noreply.github.com>
2025-12-13 10:35:12 +0800

    perubahan remote 1

--- pertemuan2/proses_compile/latihan1.c
+++ pertemuan2/proses_compile/latihan1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
+    //sekumpulan printf statement
     printf("latihan 1 untuk pertemuan 2\n");
     printf("latihan 1 untuk pertemuan 2\n");
     printf("latihan 1 untuk pertemuan 2");
