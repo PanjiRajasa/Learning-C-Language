78e2874450ed9808ffdb3cac0e8542e940e7d23a
Panji Rajasa <igustipanjirajasa@gmail.com>
2025-12-13 10:14:39 +0800

    proses compile C programs

---
+++ pertemuan2/proses _compile/latihan1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("latihan 1 untuk pertemuan 2\n");
+    printf("latihan 1 untuk pertemuan 2\n");
+    printf("latihan 1 untuk pertemuan 2");
+
+    return 0;
+}
