9a6a67c3a95afffacfbf9137640a5af495a1f1d0
Panji Rajasa <igustipanjirajasa@gmail.com>
2025-12-13 10:34:09 +0800

    perubahan lokal

--- pertemuan2/proses_compile/latihan1.c
+++ pertemuan2/proses_compile/latihan1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
+    // Sekumpulan function printf()
     printf("latihan 1 untuk pertemuan 2\n");
     printf("latihan 1 untuk pertemuan 2\n");
     printf("latihan 1 untuk pertemuan 2");
