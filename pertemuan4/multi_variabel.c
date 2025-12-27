ab0e2f812c63c8717569d094c6e4ea0f8531c045
potato obs <harris.begin.san@gmail.com>
2025-12-27 18:25:27 +0800

    learn multi variable

---
+++ pertemuan4/multi_variabel.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    float pi = 3.14, gravity = 9.8, mass = 5.5;
+
+    printf("Contoh penggunaan multi variabel: %.2f, %.2f, dan %.2f\n", pi, gravity, mass);
+
+    return 0;
+}
