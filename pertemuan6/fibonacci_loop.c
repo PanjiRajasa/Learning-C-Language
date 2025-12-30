1fbb95c072201f7613ddd092ba468dcf7ae240e6
potato obs <harris.begin.san@gmail.com>
2025-12-30 11:35:20 +0800

    fibonacci by using for loop

---
+++ pertemuan6/fibonacci_loop.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int angka1 = 0, angka2 = 1; //dua angka awal fibonacci
+
+    //variable akumulator untuk menyimpan nilai hasil penjumlahan per iterasi
+    int sum = 0;
+
+    //loop yang berjalan 18 kali
+    for(int i = 0; i < 17; i++) {
+        sum = angka1 + angka2;
+        printf("%d\n", sum);
+        angka1 = angka2;
+        angka2 = sum;
+    }
+
+    return 0;
+}
